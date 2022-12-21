ad84a8eaaca32e315a3924dbf29342eb6f832af5
HollywoodOKC <T_Lane11@yahoo.com>
2022-12-20 22:30:05 -0600

    Working.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+let port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+  let friends = ["Nitin", "Eric", "Cameron", "Riley"];
+  res.status(200).send(friends);
+})
+app.get("/weather/:temperature", (req, res) => {
+  const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+  res.status(200).send(phrase);
+})
+
+app.listen(port, () => console.log(`Server running on Port ${port}`));
